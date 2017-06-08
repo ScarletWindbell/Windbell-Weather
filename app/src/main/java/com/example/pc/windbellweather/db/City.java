4bb21b30e1659ad21d29a917f04d60500940d2e4
unknown <>
2017-06-08 17:21:15 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/example/pc/windbellweather/db/City.java
@@ -0,0 +1,56 @@
+package com.example.pc.windbellweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by PC on 2017-06-07.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id=id;
+    }
+
+    public String getCityName()
+    {
+        return cityName;
+    }
+
+    public void setCityName(String cityName)
+    {
+        this.cityName=cityName;
+    }
+
+    public int getCityCode()
+    {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode)
+    {
+        this.cityCode=cityCode;
+    }
+
+    public int getProvinceId()
+    {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId)
+    {
+        this.provinceId=provinceId;
+    }
+
+}
